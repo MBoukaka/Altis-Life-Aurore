844c89b14da7b43b7a838ea7d8e939d05eccb66d
Heimdall25 <loicshush@hotmail.fr>
2015-03-26 00:01:12 +0100

    license fal

--- Altis_Life.Altis/Config_Master.hpp
+++ Altis_Life.Altis/Config_Master.hpp
@@ -244,6 +244,7 @@ class Licenses {
 	
 	//Familles
 	LICENSEMACRO(esco,"STR_License_esco","esco",750,false,"civ")
+	LICENSEMACRO(fal,"STR_License_fal","fal",750,false,"civ")
 	LICENSEMACRO(hool,"STR_License_hool","hool",750,false,"civ")
 	LICENSEMACRO(tchong,"STR_License_tchong","tchong",750,false,"civ")
 	LICENSEMACRO(nostra,"STR_License_nostra","nostra",750,false,"civ")
